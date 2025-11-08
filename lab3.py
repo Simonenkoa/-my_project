24db4aac52a939b0e060b9033f2295e24e7f5502
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-11-08 12:17:32 +0300

    Lab 3: Задание 3 - работа со строками

--- lab3.py
+++ lab3.py
@@ -6,3 +6,8 @@ print("1. Квадраты от 1 до 10:", squares)
 evens = [x for x in range(20) if x % 2 == 0]
 print("2. Чётные из range(20):", evens)
 
+# Задание 3: List comprehension (работа со строками)
+words = ["python", "Java", "c++", "Rust", "go"]
+long_words = [w.upper() for w in words if len(w) > 3]
+print("3. Слова длиной >3:", long_words)
+
