bafa9df6d4d3492ff597602d662de698fda8df60
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-11-08 10:40:46 +0300

    Lab 3: Задание 2 - Фильтрация

---
+++ lab3.py
@@ -0,0 +1,8 @@
+# Задание 1: List comprehension
+squares = [x**2 for x in range(1, 11)]
+print("1. Квадраты от 1 до 10:", squares)
+
+# Задание 2: List comprehension
+evens = [x for x in range(20) if x % 2 == 0]
+print("2. Чётные из range(20):", evens)
+
